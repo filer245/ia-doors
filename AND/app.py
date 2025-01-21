47e8e847816df0c7c4449c27ba5b4a32aa593492
filer245 <sergiopaniagua2006@gmail.com>
2025-01-21 23:55:19 +0100

    AND door added

---
+++ AND/app.py
@@ -0,0 +1,27 @@
+import tensorflow as tf;
+from keras import models;
+import numpy as np;
+import sys;
+
+json_file = open("./AND_model.json", 'r');
+
+loaded_model_json = json_file.read();
+
+json_file.close();
+
+loaded_model = models.model_from_json(loaded_model_json);
+
+loaded_model.load_weights("./AND_model.weights.h5");
+
+print("Modelo cargado");
+
+loaded_model.compile(loss='mean_squared_error',
+                     optimizer='adam', metrics=['binary_accuracy']);
+
+data = [int(sys.argv[1]), int(sys.argv[2])];
+
+data = np.array([data], "float32");
+
+pre = loaded_model.predict(data).round();
+
+print(int(pre[0][0]));
