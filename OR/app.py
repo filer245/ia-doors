603b5fbb7ff9d414bf42447708363c2b5f6d0054
filer245 <sergiopaniagua2006@gmail.com>
2025-01-22 15:48:35 +0100

    OR door added

---
+++ OR/app.py
@@ -0,0 +1,27 @@
+import tensorflow as tf;
+from keras import models;
+import sys;
+import numpy as np;
+
+json_file = open("./OR_model.json", 'r');
+
+loaded_model_json = json_file.read();
+
+json_file.close();
+
+loaded_model = models.model_from_json(loaded_model_json);
+
+loaded_model.load_weights("./OR_model.weights.h5");
+
+print("Modelo cargado");
+
+loaded_model.compile(loss='mean_squared_error',
+                     optimizer='adam');
+
+data = [int(sys.argv[1]), int(sys.argv[2])];
+
+data = np.array([data], "float32");
+
+pre = loaded_model.predict(data).round();
+
+print(int(pre[0][0]));
